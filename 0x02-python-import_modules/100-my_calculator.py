206e637ab1034745fe041884a7093408060e7820
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-05 20:49:16 +0100

    add task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, calculator_1
+
+    op = { "+" : "add", "-" : "sub", "*" : "mul", "/" : "div"}
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    else:
+        a = sys.argv[1]
+        b = sys.argv[3]
+        sn = sys.argv[2]
+        if sn in op:
+            print("{} {} {} = {}".format(a, sn, b, op[sn](a, b)))
