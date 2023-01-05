ff0d7d27d0e017f9865d2921d13aeb02a2dfedc1
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-05 21:10:41 +0100

    add task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     import sys
-    import calculator_1
 
-    op = {"+": "add", "-": "sub", "*": "mul", "/": "div"}
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    else:
-        a = sys.argv[1]
-        b = sys.argv[3]
-        sn = sys.argv[2]
-        if sn in list(op.keys()):
-            print("{} {} {} = {}".format(a, sn, b, op[sn](a, b)))
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
