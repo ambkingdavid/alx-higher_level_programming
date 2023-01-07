1adabc60f5c8c0d05b0cdb3de7e181bc64480e3a
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-07 12:24:29 +0100

    add task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
         for x in matrix:
-            for y in x:
-                print("{:d}".format(y), end=" ")
+            for y in range(x):
+                if y == range(x) - 1:
+                    print("{:d}".format(y), end="")
+                else:
+                    print("{:d}".format(y), end=" ")
             print("")
