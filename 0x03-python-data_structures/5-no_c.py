a7b50276db79dc765214b8082ad6c4a86acc7e06
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-07 12:10:06 +0100

    add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = ""
+    new_string = ""
     for i in my_string:
         if ord(i) != ord("C") and ord(i) != ord("c"):
             new_string += i
-    return my_string
+    return new_string
