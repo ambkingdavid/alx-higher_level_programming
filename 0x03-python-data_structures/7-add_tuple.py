4206fd45159ca2e85f765ba7e8dec325651ee9a2
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-08 10:34:00 +0100

    add task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     add_list = []
-    if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
-        for i in range(min(len(tuple_a), len(tuple_b))):
-            add_list.append(tuple_a[i] + tuple_b[i])
-        for i in range(len(tuple_a) - len(tuple_b)):
-            add_list.append(tuple_a[len(tuple_b) + i])
-        for i in range(len(tuple_b) - len(tuple_a)):
-            add_list.append(tuple_b[len(tuple_a) + i])
-        return tuple(add_list)
+    for i in range(min(len(tuple_a), len(tuple_b))):
+        add_list.append(tuple_a[i] + tuple_b[i])
+    for i in range(len(tuple_a) - len(tuple_b)):
+        add_list.append(tuple_a[len(tuple_b) + i])
+    for i in range(len(tuple_b) - len(tuple_a)):
+        add_list.append(tuple_b[len(tuple_a) + i])
+    return tuple(add_list)
