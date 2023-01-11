1ca39be71a4230a273db5ce52e829d5cda951396
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-11 11:48:29 +0100

    add task 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if __name__ == "__main__":
-        if isinstance(matrix, list):
-            new_matrix = []
-            for i in range(len(matrix)):
-                row =[]
-                for j in range(len(matrix[i])):
-                    row.append(matrix[i][j]**2)
-                new_matrix.append(row)
-        return new_matrix
-
-
-
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    if isinstance(matrix, list):
+        new_matrix = []
+        for i in range(len(matrix)):
+            row =[]
+            for j in range(len(matrix[i])):
+                row.append(matrix[i][j]**2)
+            new_matrix.append(row)
+    return new_matrix
