58ecf4ad3e443fd1472858540f1e5d6d57f205f8
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-24 20:56:36 +0100

    add task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -58,13 +58,12 @@ class Square:
             print("")
 
     def __str__(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            if i != self.__size - 1:
+                print("")
+        return ("")
