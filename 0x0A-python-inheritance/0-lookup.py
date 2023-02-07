cdf5652ddaaf7cc7bff2942912773bb4283cdfe2
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-02-07 15:48:32 +0100

    Add tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""A module that contains lookup functions"""
+
+def lookup(obj):
+    """A function that returns the attributes and methods of an object"""
+
+    print(dir(obj))
