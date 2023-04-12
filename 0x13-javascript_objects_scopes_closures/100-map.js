c1548026c8b95717130d2c290c00228c83618bd9
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-04-12 21:59:16 +0000

    fixed duplicate element index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const list = require('./100-data').list;
 
-const newList = list.map(x => x * list.indexOf(x));
+const newList = list.map((x, i) => x * i);
 
 console.log(list);
 console.log(newList);
