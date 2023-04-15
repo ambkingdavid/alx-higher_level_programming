39cd89375de4ab4216e5d6279dd77d53c821e42e
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-04-15 10:02:53 +0000

    fix indentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     search = sys.argv[4]
     cursor = db.cursor()
-    cursor.execute
-    ("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (search,))
+    cursor.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (search,))
     states = cursor.fetchall()
 
     for state in states:
