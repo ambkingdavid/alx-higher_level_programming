1f7875948bbffb023ba516fbc71043140da22811
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-05-02 20:22:00 +0100

    push

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
- Python script that takes in a letter and sends a POST request
- to http://0.0.0.0:5000/search_user with the letter as a parameter.
-"""
-
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    q = sys.argv[1]
-    data = {"letter": q}
-    req = requests.post(url, data)
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
