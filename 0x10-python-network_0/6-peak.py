a758c996cc3a3b5c24d6f5288acc5d9b86b805d4
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-05-02 18:22:19 +0100

    fixed the typo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,10 +14,10 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
 
     mid = n // 2
-    peak = list_of-integers[mid]
+    peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
-    elif peak < list_of_imtegers[mid - 1]:
+    elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid+1:])
