1dd0982e796a939f3fdc72bb6f63fe7d5ca5f8b9
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-05-02 19:31:13 +0100

    push

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""function that finds a peak in a list of unsorted integers."""
+"""
+Prints peak value
+"""
+
 
 def find_peak(list_of_integers):
-    """returns the peak of list of unsorted integers"""
-    if list_of_integers == []:
+    """
+    Returns peak
+    """
+    if len(list_of_integers) == 0:
         return None
-
-    n = len(list_of_integers)
-
-    if n == 1:
-        return list_of_integers[0]
-    elif n == 2:
-        return max(list_of_integers)
-
-    mid = n // 2
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid+1:])
+    long = len(list_of_integers) - 1
+    small = 0
+    while small < long:
+        mid = (long + small) // 2
+        if list_of_integers[mid] < list_of_integers[mid+1]:
+            small = mid + 1
+        else:
+            long = mid
+    return list_of_integers[small]
